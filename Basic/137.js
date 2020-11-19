24eb7cad491f93b00342cfd280119b24cfb23272
elzodxon <sharofaddinov01@mail.ru>
2020-11-19 15:23:29 +0500

    Added 137.js

---
+++ Basic/137.js
@@ -0,0 +1,13 @@
+
+
+const fifteen = num => {
+    if(num > 15){
+        return num;
+    } else if (num === 15){
+        return 15;
+    } else {
+        return "bye bye"
+    }
+}
+
+console.log(fifteen(1))
