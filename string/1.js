80c64f5dbf51369148d03c87c7a43c81efd8fd92
elzodxon <sharofaddinov01@mail.ru>
2020-11-23 12:32:24 +0500

    added 1.js and 2.js(string/text)

--- string/1.js
+++ string/1.js
@@ -1,3 +1,5 @@
+// 1. Write a JavaScript function to check whether an `input` is a string or not.
+
 const stringOrNot = input => {
     if (typeof input == 'string') return true;
     return false;
---
+++ string/2.js
@@ -0,0 +1,11 @@
+// 2. Write a JavaScript function to check whether a string is blank or not.
+
+const blank = input => {
+    if (input.length === 0){
+        return true;
+    } else {
+        return false
+    }
+}
+
+console.log(blank('sd'))
