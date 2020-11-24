b8241417d5daa66ededbac53f2b1b4cbd7484a9b
elzodxon <sharofaddinov01@mail.ru>
2020-11-24 10:48:55 +0500

    added 25.js(basic)

--- Basic/25.js
+++ Basic/25.js
@@ -2,8 +2,11 @@
 // whether a given positive number is a multiple of 3 or a multiple of 7.
 
 const checkNumber = n => {
-    if (n % 3 === 0 || n % 7 === 0) return true;
-    return false;
+    if (n > 0){
+        if (n % 3 === 0 || n % 7 === 0) return true;
+        return false;
+    } return "Please enter positive number"
+
 }
 
-console.log(checkNumber(12))
+console.log(checkNumber(-12))
