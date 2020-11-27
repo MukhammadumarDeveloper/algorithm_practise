46065063e8370b16a5cd2b6e8065da4fae506988
elzodxon <sharofaddinov01@mail.ru>
2020-11-27 17:27:54 +0500

    Added 17.js

---
+++ Basic/17.js
@@ -0,0 +1,15 @@
+//17. Write a JavaScript program to compute the absolute difference
+// between a specified number and 19.
+// Returns triple their absolute difference if the specified number is greater than 19.
+function diffNum(n) {
+    if (n <= 19) {
+        return (19 - n);
+    }
+    else{
+        return (n - 19) * 3;
+    }
+}
+
+console.log(diffNum(12));
+console.log(diffNum(19));
+console.log(diffNum(22));
